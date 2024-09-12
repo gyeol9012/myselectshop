319669c5dab14757d4627bd76149c68e5445ff93
gyeol <gyeol9012@gmail.com>
2024-09-12 09:48:00 +0900

    폴더 별 관심상품 조회 구현

--- src/main/java/com/sparta/myselectshop/entity/Product.java
+++ src/main/java/com/sparta/myselectshop/entity/Product.java
@@ -44,7 +44,7 @@ public class Product extends Timestamped {
 
 
     @OneToMany(mappedBy = "product")
-    private List<ProductFolder> productFoldersList = new ArrayList<>();
+    private List<ProductFolder> productFolderList = new ArrayList<>();
 
     public Product(ProductRequestDto requestDto, User user) {
         this.title = requestDto.getTitle();
